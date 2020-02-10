741f493b4ce561e286cc3483f0fcf7cc638f1b7d
Uchechi090 <uchechiegbuta@gmail.com>
2020-02-10 06:43:00 +0100

    added a comment

--- src/tests/UnitTests.test.js
+++ src/tests/UnitTests.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { create } from "react-test-renderer";
-import renderer from "react-test-renderer";
+//import renderer from "react-test-renderer";
 
 import Keys from "../components/Keys";
 import { Add, Subtract, Multiply } from "../components/TriggerFunctions";
